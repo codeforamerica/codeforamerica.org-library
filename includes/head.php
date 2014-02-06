d4b6c247b13548e7c7ec5c0342011643dbc49a50
Michal Migurski <mike@teczno.com>
2014-02-06 13:15:10 -0800

    Moved headers and footers to include files

---
+++ includes/head.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="utf-8">
+    <title>Code for America</title>
+    <link rel="stylesheet" type="text/css" href="//cloud.typography.com/6435252/678502/css/fonts.css" />
+    <link rel="stylesheet" href="http://style.codeforamerica.org/style/css/main.css">
+    <link rel="stylesheet" href="http://style.codeforamerica.org/style/css/layout.css" media="all and (min-width: 40em)">
+    <link href="http://style.codeforamerica.org/style/css/prism.css" rel="stylesheet" />
+    <script src="http://style.codeforamerica.org/script/fittext.js" type="text/javascript"></script>
+    
+    
+    <!--[if lt IE 9]>
+        <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if (lt IE 9)&(!IEMobile)]>
+    <link rel="stylesheet" href="http://style.codeforamerica.org/style/css/layout.css" media="all">
+    <![endif]-->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
---
+++ includes/header.php
@@ -0,0 +1,49 @@
+<nav class="nav-global-primary">
+  <ul class="layout-breve layout-tight">
+    <li><a href="/blog">Blog</a></li>
+    <li><a href="/library">Library</a></li>
+    <li>
+      <form class="search-global" id="search-global" action="https://www.google.com/search" method="get" role="search">
+          <input type="search" id="search-global-input" class="search-global-input" autocomplete="off" placeholder="Search" name="q" />
+          <!-- consider applying autofocus="autofocus" to input -->
+          <button class="search-global-submit" id="search-global-submit" value="www.codeforamerica.org" type="submit" name="as_sitesearch">Search</button>
+      </form>
+    </li>
+  </ul>
+</nav>
+
+<div class="masthead masthead-s">
+  <div class="masthead-hero">
+    <div class="masthead-image" 
+      style="background-image: url('{{ page.masthead-image }}')">
+    </div>
+  </div>
+  <header class="layout-semibreve masthead-header">
+    <h1 class="page-title">Library</h1>
+  </header>
+</div>
+
+<div class="global-header">  
+  <a href="/" class="global-header-logo">
+      <img src="<?= $context->base() ?>/assets/logo.gif" />
+  </a>
+  <p class="skip-to-nav"><a href="#global-footer">Menu</a></p>
+
+  <nav class="nav-global-secondary">
+    <ul>
+      <li class="nav-tier1 nav-has-children">
+        <a href="/about">About</a>
+      </li>
+      <li class="nav-tier1 nav-has-children">
+        <a href="/cities">Governments</a>
+      </li>
+      <li class="nav-tier1 nav-has-children">
+        <a href="/geeks">Citizens</a>  
+      </li>
+      <li class="nav-tier1">
+        <a href="/apps">Apps</a>
+      </li>
+      <li><a href="/donate" class="button">Donate</a></li>
+    </ul>
+  </nav>    
+</div>
