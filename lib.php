f7a40117755c028739473dd7d33034c7a1491748
Michal Migurski <mike@teczno.com>
2014-02-05 23:14:53 -0800

    Simplified item selection with select() and selectf() methods

--- lib.php
+++ lib.php
@@ -10,6 +10,26 @@
             $this->dbh = new PDO("sqlite:{$dbname}");
         }
         
+       /**
+        * Run a query, fetch results list as associative arrays, and return.
+        */
+        function select($q)
+        {
+            return $this->dbh->query($q, PDO::FETCH_ASSOC);
+        }
+        
+       /**
+        * Call select() with the same parameters as sprintf: format + args list.
+        */
+        function selectf($format)
+        {
+            $args = array_map(array($this->dbh, 'quote'), func_get_args());
+            $args[0] = $format; // don't quote the format string
+            $query = call_user_func_array('sprintf', $args);
+
+            return $this->select($query);
+        }
+        
         function path_info()
         {
             return urldecode(ltrim($_SERVER['PATH_INFO'], '/'));
@@ -54,11 +74,12 @@
     function get_categories(&$ctx)
     {
         $categories = array();
+
         $query = 'SELECT DISTINCT category
                   FROM items WHERE category IS NOT NULL AND category != ""
                   ORDER BY category';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
+        foreach($ctx->select($query) as $row)
         {
             $categories[] = $row['category'];
         }
@@ -69,11 +90,12 @@
     function get_tags(&$ctx)
     {
         $tags = array();
+
         $query = 'SELECT DISTINCT tag
                   FROM item_tags WHERE tag IS NOT NULL AND tag != ""
                   ORDER BY tag';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
+        foreach($ctx->select($query) as $row)
         {
             $tags[] = $row['tag'];
         }
@@ -84,11 +106,12 @@
     function get_programs(&$ctx)
     {
         $programs = array();
+
         $query = 'SELECT DISTINCT program
                   FROM item_programs WHERE program IS NOT NULL AND program != ""
                   ORDER BY program';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
+        foreach($ctx->select($query) as $row)
         {
             $programs[] = $row['program'];
         }
@@ -99,11 +122,12 @@
     function get_locations(&$ctx)
     {
         $locations = array();
+
         $query = 'SELECT DISTINCT location
                   FROM item_locations WHERE location IS NOT NULL AND location != ""
                   ORDER BY location';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
+        foreach($ctx->select($query) as $row)
         {
             $locations[] = $row['location'];
         }
@@ -113,67 +137,47 @@
     
     function get_category_items(&$ctx, $category_name)
     {
-        $items = array();
-        $query = sprintf('SELECT * FROM items
-                          WHERE category = %s
-                          ORDER BY title',
-                         $ctx->dbh->quote($category_name));
+        $query = 'SELECT * FROM items
+                  WHERE category = %s
+                  ORDER BY title';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
-        {
-            $items[] = $row;
-        }
+        $items = $ctx->selectf($query, $category_name);
         
         return $items;
     }
     
     function get_tag_items(&$ctx, $tag_name)
     {
-        $items = array();
-        $query = sprintf('SELECT items.* FROM item_tags
-                          LEFT JOIN items ON items.id = item_tags.item_id
-                          WHERE item_tags.tag = %s
-                          ORDER BY items.title',
-                         $ctx->dbh->quote($tag_name));
+        $query = 'SELECT items.* FROM item_tags
+                  LEFT JOIN items ON items.id = item_tags.item_id
+                  WHERE item_tags.tag = %s
+                  ORDER BY items.title';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
-        {
-            $items[] = $row;
-        }
+        $items = $ctx->selectf($query, $tag_name);
         
         return $items;
     }
     
     function get_program_items(&$ctx, $program_name)
     {
-        $items = array();
-        $query = sprintf('SELECT items.* FROM item_programs
-                          LEFT JOIN items ON items.id = item_programs.item_id
-                          WHERE item_programs.program = %s
-                          ORDER BY items.title',
-                         $ctx->dbh->quote($program_name));
+        $query = 'SELECT items.* FROM item_programs
+                  LEFT JOIN items ON items.id = item_programs.item_id
+                  WHERE item_programs.program = %s
+                  ORDER BY items.title';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
-        {
-            $items[] = $row;
-        }
+        $items = $ctx->selectf($query, $program_name);
         
         return $items;
     }
     
     function get_location_items(&$ctx, $location_name)
     {
-        $items = array();
-        $query = sprintf('SELECT items.* FROM item_locations
-                          LEFT JOIN items ON items.id = item_locations.item_id
-                          WHERE item_locations.location = %s
-                          ORDER BY items.title',
-                         $ctx->dbh->quote($location_name));
+        $query = 'SELECT items.* FROM item_locations
+                  LEFT JOIN items ON items.id = item_locations.item_id
+                  WHERE item_locations.location = %s
+                  ORDER BY items.title';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
-        {
-            $items[] = $row;
-        }
+        $items = $ctx->selectf($query, $location_name);
         
         return $items;
     }
@@ -181,12 +185,12 @@
     function get_item_tags(&$ctx, $item_id)
     {
         $tags = array();
-        $query = sprintf('SELECT tag FROM item_tags
-                          WHERE item_id = %s AND tag != ""
-                          ORDER BY tag',
-                         $ctx->dbh->quote($item_id));
+
+        $query = 'SELECT tag FROM item_tags
+                  WHERE item_id = %s AND tag != ""
+                  ORDER BY tag';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
+        foreach($ctx->selectf($query, $item_id) as $row)
         {
             $tags[] = $row['tag'];
         }
@@ -197,12 +201,12 @@
     function get_item_locations(&$ctx, $item_id)
     {
         $locations = array();
-        $query = sprintf('SELECT location FROM item_locations
-                          WHERE item_id = %s AND location != ""
-                          ORDER BY location',
-                         $ctx->dbh->quote($item_id));
+
+        $query = 'SELECT location FROM item_locations
+                  WHERE item_id = %s AND location != ""
+                  ORDER BY location';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
+        foreach($ctx->selectf($query, $item_id) as $row)
         {
             $locations[] = $row['location'];
         }
@@ -213,12 +217,12 @@
     function get_item_programs(&$ctx, $item_id)
     {
         $programs = array();
-        $query = sprintf('SELECT program FROM item_programs
-                          WHERE item_id = %s AND program != ""
-                          ORDER BY program',
-                         $ctx->dbh->quote($item_id));
+
+        $query = 'SELECT program FROM item_programs
+                  WHERE item_id = %s AND program != ""
+                  ORDER BY program';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
+        foreach($ctx->selectf($query, $item_id) as $row)
         {
             $programs[] = $row['program'];
         }
@@ -228,10 +232,9 @@
     
     function get_item(&$ctx, $item_id)
     {
-        $query = sprintf('SELECT * FROM items WHERE id = %s LIMIT 1',
-                         $ctx->dbh->quote($item_id));
+        $query = 'SELECT * FROM items WHERE id = %s LIMIT 1';
         
-        foreach($ctx->dbh->query($query, PDO::FETCH_ASSOC) as $row)
+        foreach($ctx->selectf($query, $item_id) as $row)
         {
             $row['tags'] = get_item_tags($ctx, $item_id);
             $row['locations'] = get_item_locations($ctx, $item_id);
