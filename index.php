422e466a8b6f143168083652dff17968e5e4c3ac
ladyson <lauren@codeforamerica.org>
2014-05-13 14:42:04 -0700

    Added link to form to suggest a resource.

--- index.php
+++ index.php
@@ -27,7 +27,8 @@
 	
     <h2>Library <i>(Alpha)</i></h2>
     
-    <p>Welcome to Code for America's resource library, version 0.5. You'll find many videos, how-to guides, and other documents, categorized by topic. We'll be making everything prettier in late February and March.</p>
+    <p>Welcome to Code for America's resource library, version 0.5. You'll find many videos, how-to guides, and other documents, categorized by topic. </p>
+    <p><strong>You can add to the library!</strong> We want to add your slide decks, videos, documentation, and other useful materials! To be included, a resource should be relevant to the Code for America community (e.g. related to topics of civic technology and innovation) and evergreen (e.g. it will still be useful in 2+ years). <a href="https://codeforamerica.wufoo.com/forms/library-submission-form/">Submit your resources here.</a></p>
     
     <div class="layout-minim">
         <h3>Categories</h3>
