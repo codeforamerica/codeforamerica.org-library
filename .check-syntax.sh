ffb6c33e4075b21ece00e6d5530e8ac8209e29f7
Michal Migurski <mike@teczno.com>
2014-06-20 12:59:48 -0700

    Attempting to perform basic syntax checks a different way

--- .check-syntax.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh -ex
-php index.php > /dev/null
-php category.php > /dev/null
-php item.php > /dev/null
-php lib.php > /dev/null
-php location.php > /dev/null
-php person.php > /dev/null
-php program.php > /dev/null
-php tag.php > /dev/null
